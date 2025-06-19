04def8e7cbe6c9fdbfcfb287c09be43a1c291803
sufyan-pls <sufyan@planlabsolutions.com>
2025-06-19 14:16:14 +0500

    View Detail Transaction Details Modal UI done

--- src/pages/Users/UserDetailPage/Components/RentInfoCard.tsx
+++ src/pages/Users/UserDetailPage/Components/RentInfoCard.tsx
@@ -1,8 +1,11 @@
+import React, { useState } from "react";
 import { rentInformationData } from "common/data/users";
 import { Link } from "react-router-dom";
 import { Row, Col } from "reactstrap";
+import ViewDetailTransactionDetailModal from '../Modals/ViewDetailTransactionDetailModal';
 
 const RentInfoCard = () => {
+    const [modalOpen, setModalOpen] = useState(false);
     return (
         <div className="border p-3 h-100">
             <Row className="mb-2 align-items-center">
@@ -23,11 +26,12 @@ const RentInfoCard = () => {
             </Row>
             <Row>
                 <Col xs={12} className="text-end">
-                    <Link to="#" className="text-primary text-decoration-underline" style={{ cursor: "pointer" }}>
+                    <Link to="#" className="text-primary text-decoration-underline" style={{ cursor: "pointer" }} onClick={() => setModalOpen(true)}>
                         View Details
                     </Link>
                 </Col>
             </Row>
+            <ViewDetailTransactionDetailModal isOpen={modalOpen} toggle={() => setModalOpen(false)} />
         </div>
     );
 };
