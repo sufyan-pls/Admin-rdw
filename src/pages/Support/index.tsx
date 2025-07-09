a744d8559830b9e1f5ee587a7e478de1888c8bd3
sufyan-pls <sufyan@planlabsolutions.com>
2025-07-09 15:19:53 +0500

    delete unused pages

--- src/pages/Support/index.tsx
+++ src/pages/Support/index.tsx
@@ -1,10 +1,10 @@
 import BreadCrumb from "Components/Common/BreadCrumb";
 import DocumentsTable from "pages/BuilderDetail/DocumentsTable";
-import Widgets from "pages/DashboardCrm/Widgets";
 import SettingsPage from "pages/Settings/components";
 import React from "react";
 import { Container, Row } from "reactstrap";
 import SupportSection from "./components";
+// import SupportSection from "./components";
 // import Widgets from './Widgets';
 
 const SupportUI = () => {
@@ -14,9 +14,6 @@ const SupportUI = () => {
       <div className="page-content">
         <Container fluid>
           <BreadCrumb title="Support" pageTitles={[{ label: "RealWealtdy", path: "/" }, { label: "Support" }]} />
-          {/* <Row>
-            <Widgets />
-          </Row> */}
 
           <SupportSection />
         </Container>
